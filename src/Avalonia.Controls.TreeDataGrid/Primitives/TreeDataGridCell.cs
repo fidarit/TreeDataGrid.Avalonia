2590eca907b676d952c40e14b78b478c9c6ecb6a
Fernando Lucas <info@fernandolucas.info>
2026-03-27 08:33:33 +0100

    v12 rc1

--- src/Avalonia.Controls.TreeDataGrid/Primitives/TreeDataGridCell.cs
+++ src/Avalonia.Controls.TreeDataGrid/Primitives/TreeDataGridCell.cs
@@ -143,7 +143,7 @@ namespace Avalonia.Controls.Primitives
                 _treeDataGrid.RaiseCellPrepared(this, ColumnIndex, RowIndex);
         }
 
-        protected override void OnLostFocus(RoutedEventArgs e)
+        protected override void OnLostFocus(FocusChangedEventArgs e)
         {
             base.OnLostFocus(e);
 
